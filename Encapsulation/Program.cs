30b9f712c99cc365e19b2acc38ffd602e1a20575
zin pwint phyu <zinnpwintphyu5ct15@gmail.com>
2024-11-11 15:41:10 +0630

    initial commit

---
+++ Encapsulation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace EncapsulationP
+{
+     class Program
+    {
+        public static void Main(string[] args) {
+            var stuInfo = new Student();
+            //stuName.SetStudentInfo("S001", 23, "s001pp@gmail.com");
+            //Console.WriteLine(stuName);
+            stuInfo.SetStuName(" ");
+            stuInfo.SetRollNo("se-11");
+            stuInfo.SetEmail("hmone@gmail.com");
+            stuInfo.SetAge(23);
+            Console.WriteLine(stuInfo);
+            
+                }
+    }
+}
---
+++ Encapsulation/Student.cs
@@ -0,0 +1,49 @@
+﻿public class Student
+{
+    private string stuname;
+    private string rollNo;
+    private string email;
+    private int age;
+    
+    public void SetStudentInfo(string rNum,int stuAge,string stuEmail)
+    {
+        rollNo = rNum;
+        age = stuAge;
+        email = stuEmail;
+        
+    }
+    public void SetRollNo(string rno)
+    {
+        if (string.IsNullOrEmpty(rno))
+        {
+            throw new ArgumentNullException("require rollNo");
+        }
+          rollNo=rno;
+    }
+    public void SetStuName(string name)
+    {
+        if (string.IsNullOrEmpty(name)) {
+        throw new ArgumentNullException("name is required ");
+        }
+         stuname = name;
+    }
+    public void SetAge(int stuAge)
+    {
+        if (age < 0 || age > 100) {
+            throw new ArgumentOutOfRangeException("Invalid Age");
+        }
+        age = stuAge;
+    }
+    public void SetEmail(string stuEmail)
+    {
+        if (string.IsNullOrEmpty(stuEmail) || !stuEmail.Contains("@")) {
+            throw new ArgumentException("require Email Or Invalid Email");
+        }
+        email = stuEmail;
+    }
+    public override string ToString()
+    {
+        return stuname+" "+rollNo+ " "+age+" "+email;
+    }
+    
+}
